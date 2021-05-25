0e59b2eddef9efce5347b382bc67840b4f1bb6f6
Ad Astra!!! <70359572+tusharb05@users.noreply.github.com>
2021-05-25 09:41:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,27 +3,11 @@ import './App.css';
 import Row from './Row';
 import requests from './requests';
 import Banner from './Banner';
-// import Nav from './Nav'
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      {/* <h1>Hello, world!</h1> */}
-      {/* <Nav /> */}
+      
       <Banner />
       <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
