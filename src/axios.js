197b8ed20ea90389fe110822ac79f61ad6f0f72b
Ad Astra!!! <70359572+tusharb05@users.noreply.github.com>
2021-05-25 09:43:03 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
 
-//base url to make requests
 const instance = axios.create({
     baseURL: "http://api.themoviedb.org/3"
 });
-//instance.get('/something') will make a get request to address: baseURL + '/something' or http://api.themoviedb.org/3/something
 export default instance;
-//1 file can have only 1 default export
